7ee5f2bf5f485ffe406c284a347ca7ce676e662c
ShandongPolytechnicCollege <BRICSsdlgzyxy@gmail.com>
2024-09-27 11:07:32 +0300

    Update yolov5_tracking.sh

--- CODE/Click Tracking/yolov5_tracking.sh
+++ CODE/Click Tracking/yolov5_tracking.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # 脚本描述：点击二维码无人机跟踪
 
-# 该启动命令为配合SpireCV-x86推流使用，所以需要安装SpireCV，如果没有安装将导致启动失败从而导致推流失败
-gnome-terminal -- bash -c "~/SpireCV/ZLM/startMediaServer.sh; exec bash"
+# 该启动命令为配合OPENCV-x86推流使用，所以需要安装OPENCV，如果没有安装将导致启动失败从而导致推流失败
+gnome-terminal -- bash -c "~/OPENCV/ZLM/startMediaServer.sh; exec bash"
 
 sleep 1.8s
 
@@ -12,7 +12,7 @@ echo “prometheus_yolov5_tracking  successfully started”
 # 两个roslauch之间需要间隔一段时间，否则会相互抢占roscore,导致其中一个roslaunch失败,报runid错误
 sleep 0.9s  
 
-gnome-terminal -- bash -c "roslaunch spirecv_ros car_detection_with_d435i.launch; exec bash"
+gnome-terminal -- bash -c "roslaunch opencv_ros car_detection_with_d435i.launch; exec bash"
 
-echo “spirecv_object_detection  successfully started”
+echo “opencv_object_detection  successfully started”
 
